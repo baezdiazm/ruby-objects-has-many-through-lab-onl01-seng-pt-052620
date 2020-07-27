4f2d2f9ef6e98affa4ae70f2f107def12bc13487
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-27 22:55:08 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,5 +1,6 @@
 class Patient
-  attr_accessor :name
+
+  attr_accessor :name, :appointment, :doctor
   @@all = []
 
   def initialize(name)
@@ -9,6 +10,18 @@ class Patient
 
   def self.all
     @@all
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
   end
 
 end
